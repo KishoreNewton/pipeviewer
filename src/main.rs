e35dee8a2bc343d838d5e2f07494a95fb8b29bb5
Kishore Newton <kishorenke@gmail.com>
2022-10-11 20:11:44 +0000

    hook check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
 fn main() {
-    println!("Hello, world!");
+    if true == true {}
+    let silent = env::var("PV_SILENT").unwrap_or_default().is_empty();
+    dbg!(silent);
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => n,
+            Err(e) => panic!("Error reading stdin: {}", e),
+        };
+        dbg!(total_bytes += num_read);
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    if dbg!(!silent) {
+        eprintln!("Total bytes: {}", total_bytes);
+    }
 }
